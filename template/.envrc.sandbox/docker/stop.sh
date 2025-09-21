5fa0bfad2fd8204e2d78efa27c01f9cd73ecb6d1
Max YO <max@monstermax.net>
2025-09-22 01:36:44 +0200

    dev 2025-09

---
+++ template/.envrc.sandbox/docker/stop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+
+if [ "$VENV_CONTAINER" = "" ]; then
+    echo "Error missing VENV_CONTAINER"
+    exit 1
+fi
+
+
+# Stop docker container
+docker stop ${VENV_CONTAINER}
+
