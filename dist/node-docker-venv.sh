3db523cc16f3ac4eaf37cc6928595837d0ee1af0
Max YO <max@monstermax.net>
2025-09-22 00:16:24 +0200

    dev 2025-09

--- dist/node-docker-venv.sh
+++ dist/node-docker-venv.sh
@@ -22,6 +22,7 @@ if [ ! -d "$TARGET_DIR" ]; then
 fi
 
 # Do not overwrite any existing files (GNU tar: --keep-old-files ; BSD tar: -k)
+TAR_KEEP=""
 #TAR_KEEP="--keep-old-files"
 
 # Find the line where the archive starts and then extract
