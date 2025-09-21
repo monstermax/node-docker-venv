caf82c845851d6893f6a200447d2fb835b2fdfca
Max YO <max@monstermax.net>
2025-09-22 00:22:46 +0200

    dev 2025-09

--- template_installer/installer.sh
+++ template_installer/installer.sh
@@ -22,15 +22,15 @@ if [ ! -d "$TARGET_DIR" ]; then
 fi
 
 # Do not overwrite any existing files (GNU tar: --keep-old-files ; BSD tar: -k)
-#TAR_KEEP=""
-TAR_KEEP="--keep-old-files"
+TAR_KEEP=""
+#TAR_KEEP="--keep-old-files"
 
 # Find the line where the archive starts and then extract
 MARK="__ARCHIVE_BELOW__"
 LINE=$(awk "/^$MARK$/{print NR+1; exit 0}" "$0")
 
 echo "[sandbox] extraction in: $TARGET_DIR"
-tail -n +$LINE "$0" | tar -xz $TAR_KEEP -C "$TARGET_DIR"
+tail -n +$LINE "$0" | tar -xz ${TAR_KEEP-} -C "$TARGET_DIR"
 
 echo "[sandbox] done. (if .envrc present: 'direnv allow')"
 exit 0
